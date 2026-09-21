a54232e8efbdebe013193d0ab94c5d9065b4ffb4
Artem Bas <mrfoxybas@gmail.com>
2026-09-21 23:35:23 +0200

    Fixed typo in the discography description

--- src/update/summary.js
+++ src/update/summary.js
@@ -258,7 +258,7 @@ function generateDiscographySummary() {
   if (bestSinceAlbums.length === 1 && biggestGainer && bestSinceAlbums[0].title === biggestGainer.title && biggestGainer.isPositive) {
     usePoint3_1 = true;
     const cleanPercent = biggestGainer.displayPercent.replace('+', '');
-    summaryLines.push(`• ${biggestGainer.title} was the biggest ganer and scored its best day since ${bestSinceAlbums[0].dateStr} (up ${cleanPercent} from yesterday).`);
+    summaryLines.push(`• ${biggestGainer.title} was the biggest gainer and scored its best day since ${bestSinceAlbums[0].dateStr} (up ${cleanPercent} from yesterday).`);
   }
 
   if (!usePoint3_1) {
